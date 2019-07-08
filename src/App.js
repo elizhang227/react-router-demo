efc598f2c352aca23bb4f3a976632c76c3fa8720
elizhang227 <zhang.eli@hotmail.com>
2019-07-08 10:00:10 -0400

    made 3 routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import './App.css';
+
+
+const Index = props => {
+  console.log('props are', props)
+  return (
+    <>
+      <h2>Home Page</h2>
+      <p>Hello {props.name}</p>
+    </>
+  )
+}
+const About = () => <h2>About Page</h2>;
+const Contact = () => <h2>Contact Page</h2>;
 
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+          </ul>
+        </nav>
 
-    </div>
+        <Route 
+          path='/' 
+          exact 
+          render={routeProps => <Index name='Eli' {...routeProps} /> }
+        />
+        <Route path='/about/' component={About} />
+        <Route path='/contact/' component={Contact} />
+      </div>
+    </Router>
   );
 }
 
